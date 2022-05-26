ecd71e14de3af165f860cd376200e7b5c04e9360
Ethan Blake <ethanblake417@gmail.com>
2022-05-26 12:28:18 -0700

    First Commit

---
+++ website.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home_page.html")
+def home():
+    return render_template("home_page.html")
+
+
+@app.route("/about_me.html")
+def about_me():
+    return render_template("about_me.html")
+
+
+@app.route("/programming_experience.html")
+def programming_experience():
+    return render_template("programming_experience.html")
+
+
+# @app.route("/projects.html")
+# def projects():
+#     return render_template("projects.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
